4a2e25935e9484838e9e968d1603a3c08f7756ba
Huzaifa Hussnain <huzaifa.hussnain@square63.org>
2019-07-08 18:26:29 +0500

    Displayed the profile picture while editing the profile and added validations to not update the picture without providing the current password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :update_permitted_parameters, if: :devise_controller?
-
+  before_action :validate_resource, if: :devise_controller?, only: [:update]
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
@@ -10,5 +10,14 @@ class ApplicationController < ActionController::Base
   def update_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
   end
+
+  private
+  def validate_resource
+   unless update_resource(resource, account_update_params.except(:avatar))
+     clean_up_passwords resource
+     set_minimum_password_length
+     respond_with resource
+   end
+ end
 
 end
