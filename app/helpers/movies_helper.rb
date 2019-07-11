9b74c43ccf217de003e40c29c275c48a6a708d27
Huzaifa Hussnain <huzaifa.hussnain@square63.org>
2019-07-11 17:48:10 +0500

    Allowed the admin to post a review for a movie

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -7,10 +7,6 @@ module MoviesHelper
     user_signed_in? && current_user.admin
   end
 
-  def user_logged_in?
-    user_signed_in? && !current_user.admin
-  end
-
   def allow_review_deletion?(review)
     user_signed_in? && (current_user.id ==  review.user.id || current_user.admin)
   end
