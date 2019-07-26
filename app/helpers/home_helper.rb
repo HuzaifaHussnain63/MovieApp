f162e42d95ec14f625c581bcb95b1a89ef5f46af
Huzaifa Hussnain <huzaifa.hussnain@square63.org>
2019-07-26 15:51:35 +0500

    Showing the tabs in homepage automatically that have some data

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def show_nav_item?(item1, item2)
+    return 'active' if item1 && item1.none? && item2 && item2.none?
+  end
+
+  def show_tab_pane?(item1, item2)
+     return 'active show' if item1 && item1.none? && item2 && item2.none?
+  end
 end
